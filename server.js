2e569f577643e3ff02d5cc99932449295e8514f2
Antonin <antonin.sous@gmail.com>
2026-09-05 20:10:54 +0200

    Adapter le serveur à Render

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ const nodemailer = require("nodemailer");
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 
 // ============================================================
 // VARIABLES ENV
